7e86d6dd2297ced112d837184600033b404c3dee
William <68653853+will-i-amv@users.noreply.github.com>
2023-10-10 13:25:49 -0300

    Read env variables from .env file

--- config/settings/base.py
+++ config/settings/base.py
@@ -5,15 +5,12 @@ from pathlib import Path
 
 import environ
 
+
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 # portfolio_builder/
 APPS_DIR = BASE_DIR / "portfolio_builder"
 env = environ.Env()
-
-READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=False)
-if READ_DOT_ENV_FILE:
-    # OS environment variables take precedence over variables from .env
-    env.read_env(str(BASE_DIR / ".env"))
+env.read_env(str(BASE_DIR / ".env"))
 
 # GENERAL
 # ------------------------------------------------------------------------------
