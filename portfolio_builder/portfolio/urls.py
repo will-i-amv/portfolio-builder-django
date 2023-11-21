8c9281a1134034e1096e5b21d717888a1fb3d778
William <68653853+will-i-amv@users.noreply.github.com>
2023-11-21 10:00:02 -0300

    Don't use exact match for default URL, otherwise the URL is never matched

--- portfolio_builder/portfolio/urls.py
+++ portfolio_builder/portfolio/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path(r"^(?P<portf_name>\w+)/add_pos/$", portf_views.add_position, name="add_pos"),
     path(r"^(?P<portf_name>\w+)/(?P<ticker>\w+)/upd_pos/$", portf_views.upd_position, name="upd_pos"),
     path(r"^(?P<portf_name>\w+)/(?P<ticker>\w+)/del_pos/$", portf_views.del_position, name="del_pos"),
-    path(r"^$", portf_views.index, name="index"),
+    path(r"", portf_views.index, name="index"),
 ]
